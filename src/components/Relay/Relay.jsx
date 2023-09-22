e5ba282d519c535e575be2b5bd9e21c805cabb22
zakiaatot <384637134@qq.com>
2023-09-22 09:10:34 +0800

    更新转发提示，新增长按图片转发方式

--- src/components/Relay/Relay.jsx
+++ src/components/Relay/Relay.jsx
@@ -1,5 +1,6 @@
-import { showDialog } from 'vant'
+import { Dialog } from 'vant'
 import 'vant/es/dialog/style'
+import './Relay.css'
 
 export default {
   props: {
@@ -9,15 +10,13 @@ export default {
     }
   },
   data() {
-    return {}
+    return {
+      showFlag: false
+    }
   },
   methods: {
     clickHandle() {
-      showDialog({
-        title: '快喊上小伙伴一起玩耍吧~',
-        message: '点击右上角分享到朋友圈、QQ空间或分享给微信、QQ好友, 投掷次数将 +1 (每日限3次)',
-        theme: 'round-button'
-      })
+      this.showFlag = true
     }
   },
   mounted() {},
@@ -27,6 +26,18 @@ export default {
         <button onClick={this.clickHandle}>
           <img className="inline-block" src="/icon/relay.svg" alt="relay" />
         </button>
+        <Dialog theme="round-button" v-model:show={this.showFlag} close-on-click-overlay>
+          <div className="relay-title w-full h-8 mt-4 mb-2 text-center text-xl">
+            快喊上小伙伴一起玩耍吧~
+          </div>
+          <div className="w-full text-center p-2">
+            点击右上角或长按图片分享到朋友圈、QQ空间或分享给微信、QQ好友, 投掷次数+1 <br />
+            (每日限3次)
+          </div>
+          <div className=" aspect-square w-full p-2">
+            <img src="/img/qrcode.png" className=' rounded-2xl' alt="qrcode" />
+          </div>
+        </Dialog>
       </div>
     )
   }
